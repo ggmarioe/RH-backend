4fe47b37476eeaefa7098356dcde7f7781b876d6
mguajardo_mvc <mguajardo.eecc@mineravallecentral.cl>
2022-03-01 17:32:33 -0300

    creación de modelos de datos

--- backend/setup/models.py
+++ backend/setup/models.py
@@ -4,3 +4,25 @@ from django.db import models
 # Create your models here.
 class CustomUser(AbstractUser):
     contract_state= models.CharField(max_length=5, blank=True, null=True)
+
+
+
+# clase para definir los documentos que se van a subir
+class documents(models.Model):
+    pass
+
+# clase para definir la asocicación entre los documentos y los usuarios
+class documentsUsers(models.Model):
+    pass
+
+
+# clase para definir una rendición de cuenta
+class expenses(models.Model):
+    pass
+
+# clase de asociación entre la rendición de cuenta y los usuarios
+class expensesUsers(models.Model):
+    pass
+
+
+
